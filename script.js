1bd9a9f0d54a85222dbb79e0463b80565f9006bd
Carolina Turner <carolinanturner@gmail.com>
2024-02-18 10:56:47 -0500

    message

---
+++ script.js
@@ -0,0 +1,49 @@
+const getBrothers = async ()=>{
+    try {
+        const response = await fetch("/json/brothers.json");
+        return await response.json();
+    }catch (error){
+        console.log(error);
+    }
+};
+
+const showBrothers = async () =>{
+    let brothers = await getBrothers();
+    let brothersSection=document.getElementById("brothers-section");
+    brothers.forEach((brother) => {
+        brothersSection.append(getBrothers(brother));
+    });
+  
+};
+
+const getBrotherBio=(brother)=>{
+    let section=document.createElement("section");
+
+    let h3=document.createElement("h3");
+    h3.innerText=brother.name;
+    section.append(h3);
+
+    let ul = document.createElement("ul");
+    section.append(ul);
+    ul.append(getLi(`Hometown: ${brother.hometown}`));
+    ul.append(getLi(`Major(s): ${brother.majors}`));
+    ul.append(getLi(`Minor(s): ${brother.minors}`));
+    ul.append(getLi(`Bio: ${brother.bio}`));
+    ul.append(getLi(`LinkedIn: ${brother.linkedIn}`));
+    ul.append(getLi(`Resume: ${brother.resume}`));
+    section.classList.add("brother");
+
+    return section;
+}
+
+
+const getLi = data=>{
+    const li =document.createElement("li");
+    li.textContent= data;
+    return li;
+}
+
+
+window.onload= ()=>{
+    showCourses();
+}
