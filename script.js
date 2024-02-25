a054643d4eeaa982c6c2c5e080f85033acef3ad9
Carolina Turner <carolinanturner@gmail.com>
2024-02-25 03:44:46 -0500

    commit :(

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const getBrothers = async ()=>{
     try {
         console.log("hello");
         const response = await fetch("https://thetatauzd.github.io/json/brothers.json");
-        return await response.json();
+        return response.json();
     } catch(error){
         console.log(error);
     }
